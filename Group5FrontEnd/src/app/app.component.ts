572f5178fe1187a508f134e77af9d204a5f4dc21
kimjin <jinente@udel.edu>
2020-05-16 18:40:35 -0400

    new

--- Group5FrontEnd/src/app/app.component.ts
+++ Group5FrontEnd/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Group5FrontEnd';
   activeClass = 'active';
-  activeClassR = 'activeClassR';
-  dark = true;
+  get loggedIn():boolean{
+    return this.authSvc.loggedIn;
+  }
+  constructor(public authSvc:AuthService) {
+    authSvc.authorize();
+  }
+
+
+  signout(){
+    this.authSvc.logout();
+    return false;
+  }
 }
+
--- Group5FrontEnd/src/app/app.module.ts
+++ Group5FrontEnd/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
