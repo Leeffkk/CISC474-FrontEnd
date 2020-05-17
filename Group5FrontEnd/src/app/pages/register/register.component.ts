332bb4532256b316cdf2a3102acf4cf023bf2891
kimjin <jinente@udel.edu>
2020-05-17 14:37:40 -0400

    new

--- Group5FrontEnd/src/app/pages/register/register.component.ts
+++ Group5FrontEnd/src/app/pages/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  loading =false;
+  submitted=false;
+  returnUrl: string;
+  error: string;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,private route: ActivatedRoute,private router: Router,private authSvc:AuthService) {
+      if (authSvc.loggedIn)
+      this.router.navigate(['/']);
+   }
 
   ngOnInit(): void {
+    this.registerForm=this.formBuilder.group({
+      username: ['',Validators.required],
+      password: ['',Validators.required]
+    });
+    this.returnUrl=this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
+  regist(){
+    this.submitted=true;
+    if (this.registerForm.invalid){
+      return;
+    }
+    this.loading=true;
+    this.authSvc.login(this.registerForm.controls.username.value,this.registerForm.controls.password.value).subscribe(response=>{
+      this.router.navigate([this.returnUrl]);
+    },err=>{this.submitted=false;this.loading=false;this.error=err.message||err;});
+  }
 }
