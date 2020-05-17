8e4f0ba540f7264d589bb0958461581561887755
kimjin <jinente@udel.edu>
2020-05-17 14:20:01 -0400

    j

--- Group5FrontEnd/src/app/app.component.spec.ts
+++ Group5FrontEnd/src/app/app.component.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
+
+//platformBrowserDynamic().bootstrapModule(AppModule);
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
