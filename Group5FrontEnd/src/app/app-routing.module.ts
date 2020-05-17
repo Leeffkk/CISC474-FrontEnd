e137615cd7a16738909710c7bb5ebe66755c9785
rmelillo <rmelillo@udel.edu>
2020-05-17 14:18:54 -0400

    example stuff

--- Group5FrontEnd/src/app/app-routing.module.ts
+++ Group5FrontEnd/src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { AddprojectComponent } from './pages/addproject/addproject.component';
 
 
 const routes: Routes = [
   {path:'',redirectTo:'home',pathMatch:'full'},
-  {path:'home', component: HomeComponent},
-  {path:'about',component: AboutComponent},
-  {path:'login',component: LoginComponent}
+  {path:'home',component: HomeComponent},
+  {path:'login',component: LoginComponent},
+  {path:'register',component: RegisterComponent},
+  {path:'addproject',component: AddprojectComponent}
 ];
 
 @NgModule({
--- Group5FrontEnd/src/app/app.component.spec.ts
+++ Group5FrontEnd/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Group5FrontEnd'`, () => {
+  it(`should have as title 'ExampleUI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Group5FrontEnd');
+    expect(app.title).toEqual('ExampleUI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Group5FrontEnd app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ExampleUI app is running!');
   });
 });
